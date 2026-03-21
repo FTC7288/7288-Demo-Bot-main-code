b592bd21160635cfb63cd879dd82bc1497ac7296
nrhnck <nora@tcrobotics.org>
2026-03-21 14:39:28 -0400

    Commit Message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Nora.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Nora.java
@@ -1,9 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Nora {
-    int nora = 1;
-    double shayshay = .67;
-    char sigma = 'a';
-    String sigma2 = "abc";
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp(name = "Nora TeleOp")
+public class Nora extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        int nora = 9;
+        int shay = 12;
+        int seamus = shay + nora;
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the driver to press PLAY
+        waitForStart();
+
+        // Run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            telemetry.addData("Number", seamus);
+            telemetry.update();
+        }
+    }
 }
